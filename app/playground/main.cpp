e17fce9a406f93915edf4af4135609d0473e232a
xiaoqiyu <rpyxqi@gmail.com>
2021-11-29 21:59:30 +0800

    add consumer ,producer

--- app/playground/main.cpp
+++ app/playground/main.cpp
@@ -1,80 +1,58 @@
-#include<iostream>
-#include<vector>
-#include <thread>
-#include<cstring>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <iostream>
 #include <stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include "gmtrade_def.h"
-#include "gmtrade.h"
-#include "GMSimTrader.h"
-#include <fstream>
-#include <glog/logging.h>
-#include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/msg.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <unistd.h>
-
-#include <boost/lockfree/spsc_queue.hpp> // ring buffer
-
-#include <boost/interprocess/managed_shared_memory.hpp>
-#include <boost/interprocess/allocators/allocator.hpp>
-#include <boost/interprocess/containers/string.hpp>
-
-
-namespace bip = boost::interprocess;
-namespace shm
-{
-    typedef bip::allocator<char, bip::managed_shared_memory::segment_manager> char_alloc;
-    typedef bip::basic_string<char, std::char_traits<char>, char_alloc >      shared_string;
-
-    typedef boost::lockfree::spsc_queue<
-        shared_string, 
-        boost::lockfree::capacity<200> 
-    > ring_buffer;
-}
 
 int main(int argc, char *argv[])
-{      
-    if (argc < 2) return 0;
-    if (argv[1] == "p")
-    {
-        // create segment and corresponding allocator
-        bip::managed_shared_memory segment(bip::open_or_create, "MySharedMemory", 65536);
-        shm::char_alloc char_alloc(segment.get_segment_manager());
+{
+   using namespace boost::interprocess;
 
-        // Ringbuffer fully constructed in shared memory. The element strings are
-        // also allocated from the same shared memory segment. This vector can be
-        // safely accessed from other processes.
-        shm::ring_buffer *queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+   if(argc == 1){  //Parent process
+      //Remove shared memory on construction and destruction
+      struct shm_remove
+      {
+         shm_remove() { shared_memory_object::remove("MySharedMemory"); }
+         ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
+      } remover;
 
-        const char* messages[] = { "hello world", "the answer is 42", "where is your towel", 0 };
+      //Create a shared memory object.
+      shared_memory_object shm (create_only, "MySharedMemory", read_write);
+      
 
-        for (const char** msg_it = messages; *msg_it; ++msg_it)
-        {
-            sleep(1);
-            queue->push(shm::shared_string(*msg_it, char_alloc));
-        }
-    }else if (argv[1] == "c"){
-        // create segment and corresponding allocator
-        bip::managed_shared_memory segment(bip::open_or_create, "MySharedMemory", 65536);
-        shm::char_alloc char_alloc(segment.get_segment_manager());
+      //Set size
+      shm.truncate(20);
+    //   std::cout<<shm.get_size()<<std::endl;
+      //Map the whole shared memory in this process
+      mapped_region region(shm, read_write);
 
-        shm::ring_buffer *queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+      //Write all the memory to 1
+      std::memset(region.get_address(), 1, region.get_size());
+     char myname[] = "Pierre de Fermat";
+      std::memcpy(region.get_address(), myname, strlen(myname)+1);
 
-        while (true)
-        {
-            shm::shared_string v(char_alloc);
-            if (queue->pop(v))
-            std::cout << "Processed: '" << v << "'\n";
-        }
-    }else{
-        return 0;
-    }
+      //Launch child process
+      std::string s(argv[0]); s += " child ";
+      if(0 != std::system(s.c_str()))
+         return 1;
+   }
+   else{
+      //Open already created shared memory object.
+      shared_memory_object shm (open_only, "MySharedMemory", read_only);
 
+      //Map the whole shared memory in this process
+      mapped_region region(shm, read_only);
 
-    return 0;
+      //Check that memory was initialized to 1
+      char *mem = static_cast<char*>(region.get_address());
+      for(std::size_t i = 0; i < region.get_size(); ++i){
+        std::cout<<"mem:"<<*(mem+i)<<std::endl;
+        //  if(*mem++ != 1)
+            // return 1;   //Error checking memory
+      }
+   }
+   return 0;
 }
